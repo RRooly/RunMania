cef147aa64479fac149c458bfb6512eb22848328
RRooly <randrianasolorooly@gmail.com>
2021-11-02 20:50:35 +0400

    add basic physics
    Add basic physics on the player to make him go ahead constantly without falling through the ground

---
+++ HyperCasualGame Test/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0,0,2000 * Time.deltaTime);
+    }
+}
