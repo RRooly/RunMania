9692b7d52fc653b199318c5026ad5a3f6954b275
RRooly <randrianasolorooly@gmail.com>
2021-11-03 01:52:59 +0400

    adding switching lane & mobileInput Dev
    adding switching lane system and debut of mobile input process

---
+++ HyperCasualGame Test/Assets/Scripts/MobileInput.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobileInput : MonoBehaviour
+{
+    private const float DEADZONE = 125f;
+    public static MobileInput Instance { set; get; }
+    private bool tap, swipeLeft, swipeRight;
+    private bool isDragging = false;
+    private Vector2 startTouch, swipeDelta;
+
+    public Vector2 SwipeDelta { get { return swipeDelta; } }
+    public bool SwipeLeft { get { return swipeLeft; } }
+    public bool SwipeRight { get { return swipeRight; } }
+
+    void Awake()
+    {
+
+        Instance = this;
+
+    }
+    void Update()
+    {
+        //Reset all the booleans
+        tap = swipeLeft = swipeRight = false;
+
+        #region Standalone Inputs
+        if (Input.GetMouseButtonDown(0))
+        {
+            tap = true;
+            isDragging = true;
+            startTouch = Input.mousePosition;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            isDragging = false;
+            Reset();
+        }
+        #endregion
+
+        #region Mobile Inputs
+        if (Input.touches.Length != 0)
+        {
+            if (Input.touches[0].phase == TouchPhase.Began)
+            {
+                tap = true;
+                isDragging = true;
+                startTouch = Input.mousePosition;
+            }
+            else if (Input.touches[0].phase == TouchPhase.Ended || Input.touches[0].phase == TouchPhase.Canceled)
+            {
+                isDragging = false;
+                Reset();
+            }
+        }
+        #endregion
+
+        //Calculate the distance
+        swipeDelta = Vector2.zero;
+        if (isDragging)
+        {
+            if (Input.touches.Length != 0)
+            {
+                swipeDelta = Input.touches[0].position - startTouch;
+            }
+            else if (Input.GetMouseButtonDown(0))
+            {
+                swipeDelta = (Vector2)Input.mousePosition - startTouch;
+            }
+        }
+
+        //Did we cross the deadzone ?
+        if (swipeDelta.magnitude > 125)
+        {
+
+            //Which direction ?
+            float x = swipeDelta.x;
+            float y = swipeDelta.y;
+            if (Mathf.Abs(x) > Mathf.Abs(y))
+            {
+
+                //Left or right
+                if (x < 0)
+                {
+                    swipeLeft = true;
+
+                }
+                else
+                {
+                    swipeRight = true;
+                }
+            }
+            Reset();
+        }
+    }
+
+    void Reset()
+    {
+        startTouch = swipeDelta = Vector2.zero;
+    }
+}
--- HyperCasualGame Test/Assets/Scripts/PlayerMovement.cs
+++ HyperCasualGame Test/Assets/Scripts/PlayerMovement.cs
@@ -5,19 +5,62 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
-    public Rigidbody rb;
-
-    public float speed = 2000f;
+    private CharacterController controller;
+    public float speed = 7.0f;
+    private const float LANE_DISTANCE = 3.0f;
+    public int desiredLane = 1; //0=left ; 1=middle ; 2=right
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        rb.AddForce(0,0, speed * Time.deltaTime);
+
+
+        //move left
+        if (Input.GetKeyDown("q"))
+        {
+            SwitchLane(false);
+        }
+
+        //move right
+        if (Input.GetKeyDown("d"))
+        {
+            SwitchLane(true);
+        }
+
+        //Calculate the next position
+        Vector3 targetPosition = transform.position.z * Vector3.forward;
+        if (desiredLane == 0)
+        {
+            targetPosition += Vector3.left * LANE_DISTANCE;
+        }
+
+        else if (desiredLane == 2)
+        {
+            targetPosition += Vector3.right * LANE_DISTANCE;
+        }
+
+        Vector3 moveVector = Vector3.zero;
+        moveVector.x = (targetPosition - transform.position).x * speed;
+        moveVector.y = -0.1f;
+        moveVector.z = speed;
+
+        //Move the character
+        controller.Move(moveVector * Time.deltaTime);
+
     }
+
+    private void SwitchLane(bool goingRight)
+    {
+        //avoid player to go outside the three main line
+        desiredLane += (goingRight) ? 1 : -1;
+        desiredLane = Mathf.Clamp(desiredLane, 0, 2);
+    }
+
+
 }
