7b9f23857d5761abae650df6b8ddb2cba8a269e1
RRooly <randrianasolorooly@gmail.com>
2021-11-03 11:15:35 +0400

    health system
    adding health system that reduce through time

---
+++ HyperCasualGame Test/Assets/Scripts/Player.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public int maxHealth = 100;
+    public int currentHealth;
+    public HealthBar healthBar;
+    public int IsRunning = 1;
+    public int NumberofSeconds; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = 60;
+        healthBar.SetMaxHelth(maxHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsRunning == 1)
+        {
+            StartCoroutine(ConstantDamage());
+        }
+    }
+
+    //Function that reduce health time by time
+    public IEnumerator ConstantDamage()
+    {
+        IsRunning = 0;
+        yield return new WaitForSeconds(NumberofSeconds); 
+        TakeDamage(20);
+        IsRunning = 1;
+    }
+
+    
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
+        if (currentHealth <= 0)
+        {
+           
+        }
+    }
+
+
+
+}
