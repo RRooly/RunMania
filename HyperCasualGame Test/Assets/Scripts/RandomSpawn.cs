757caa9f5020f9022f3b45a7a74025d4cc0bf584
RRooly <randrianasolorooly@gmail.com>
2021-11-04 15:48:40 +0400

    spawning object system
    creating a spawning object system

---
+++ HyperCasualGame Test/Assets/Scripts/RandomSpawn.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSpawn : MonoBehaviour
+{
+    //Create a list that contain the position of our spawners
+    public Transform[] Position;
+    public GameObject gameObject;
+
+    public Transform Location;
+
+    public bool toSpawn = true;
+
+    void Start()
+    {
+        Location = Position[Random.Range(0, Position.Length)];
+    }
+    void Update()
+    {
+     StartCoroutine(Spawn());
+    }
+
+    IEnumerator Spawn()
+    {
+        Location = Position[Random.Range(0, Position.Length)];
+
+        if (toSpawn)
+        {
+
+            Instantiate(gameObject, Location);
+            toSpawn = false;
+            yield return new WaitForSeconds(1);
+            toSpawn = true;
+
+        }
+        
+    }
+}
