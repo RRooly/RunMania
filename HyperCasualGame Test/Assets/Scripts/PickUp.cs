ff15f507a5d98decf20fcd36d616397bc968022c
RRooly <randrianasolorooly@gmail.com>
2021-11-05 21:37:31 +0400

    moving object
    addint force to object

--- HyperCasualGame Test/Assets/Scripts/PickUp.cs
+++ HyperCasualGame Test/Assets/Scripts/PickUp.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class PickUp : MonoBehaviour
 {
+
+    public Rigidbody rb;
+    public float forwardForce = 5000f;
+
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody>();
         
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        rb.AddForce(0,0, -forwardForce * Time.deltaTime);
     }
 }
