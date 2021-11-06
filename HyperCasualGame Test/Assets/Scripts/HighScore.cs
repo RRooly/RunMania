1e74363843466a0ec56fd20fc2379e95b1c1cef5
RRooly <randrianasolorooly@gmail.com>
2021-11-07 01:06:37 +0400

    score UI update

--- HyperCasualGame Test/Assets/Scripts/HighScore.cs
+++ HyperCasualGame Test/Assets/Scripts/HighScore.cs
@@ -2,32 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-public class Score : MonoBehaviour
+
+public class HighScore : MonoBehaviour
 {
 
-    public Transform player;
-    public Text scoreText;
-    public Text highScoreText;
-
-    public float score =0;
+    public Text pointsText;
+    public Text highScore;
+    public static float score = 0;
     public float actualScore;
     // Start is called before the first frame update
     void Start()
     {
-        highScoreText.text = PlayerPrefs.GetFloat("HighScore", 0).ToString();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        actualScore = player.position.z;
-        scoreText.text = actualScore.ToString("0");
+        actualScore = (int)score;
+        pointsText.text = actualScore.ToString(); 
+        
 
+        //Permet de sauvegarder le meilleur score effectué
         if (actualScore > PlayerPrefs.GetFloat("HighScore", 0))
         {
             PlayerPrefs.SetFloat("HighScore", actualScore);
-            highScoreText.text = actualScore.ToString("0");
+            highScore.text = actualScore.ToString();
         }
+    }
+
+    public void UpdateScore()
+    {
+        
     }
 }
