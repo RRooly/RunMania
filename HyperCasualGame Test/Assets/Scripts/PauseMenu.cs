b04c8f84ae6664ee289eaefb6c6a6ad18b9bb763
RRooly <randrianasolorooly@gmail.com>
2021-11-07 08:13:01 +0400

    add pause system&UI

---
+++ HyperCasualGame Test/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool gameIsPaused;
+    public GameObject pauseMenuUI;
+
+    void Start()
+    {
+        gameIsPaused = false;
+        Time.timeScale = 1;
+    }
+
+    public void GamePause()
+    {
+        Time.timeScale = 0;
+        pauseMenuUI.SetActive(true);
+        gameIsPaused = true;
+    }
+
+    public void GameResume()
+    {
+        Time.timeScale = 1;
+        pauseMenuUI.SetActive(false);
+        gameIsPaused = false;
+    }
+
+    public void GameRestart()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void GameQuit()
+    {
+        Application.Quit();
+    }
+
+
+
+    void OnApplicationPause()
+    {
+        gameIsPaused = true;
+        Time.timeScale = 0;
+    }
+}
